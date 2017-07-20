edf9145199f0d348cdc45b036d967f33124e6576
Thng <xcodevn@gmail.com>
2017-07-20 17:37:08 +0800

    Fixed

--- play.go
+++ play.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"time"
 )
 
 type DT struct {
@@ -38,47 +39,47 @@ func main() {
 	length, _ := strconv.ParseInt(maps["Content-Length"][0], 10, 64)
 
 	f, _ := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0655)
-	f.Seek(length, 0)
+	loc, err := f.Seek(length, 0)
+
+	if err != nil || loc != length {
+		fmt.Println("SEEK ERR!")
+		return
+	}
 
 	ch := make(chan DT, 100)
 	chcon := make(chan Con)
 
-	var limit int64 = 100
+	var limit int64 = 50
 	len_sub := length / limit
 
 	cons := make([]Con, limit)
 	cons[0] = Con{0, len_sub}
-
-	cons[1] = Con{length - 1024*1024, length}
-	cons[2] = Con{length - 1024*1024, length}
-	cons[3] = Con{length - 1024*1024, length}
-	cons[4] = Con{length - 1024*1024, length}
+	cons[1] = Con{length - len_sub, length}
 
 	go func() {
 		var i int64
 		chcon <- cons[0]
 		chcon <- cons[1]
-		chcon <- cons[2]
-		chcon <- cons[3]
-		chcon <- cons[4]
 
 		for i = 2; i < limit; i++ {
 			cons[i] = Con{(i - 1) * len_sub, i * len_sub}
 			chcon <- cons[i]
-			fmt.Printf("\r  %0.2f%%   ", float64(i+1)*100.0/float64(limit))
 		}
 	}()
 
 	ncon := 6
 
-	wg.Add(int(limit + 3))
+	wg.Add(int(limit))
 
 	go func() {
+		cc := 0
 		for c := range ch {
 			f.Seek(c.At, 0)
 			f.Write(c.Data)
 			f.Sync()
 			wg.Done()
+			cc = cc + 1
+			fmt.Printf("\r  %0.2f%%   ", float64(cc)*100.0/float64(limit))
 		}
 	}()
 
@@ -92,7 +93,8 @@ func main() {
 				max := co.Max
 
 				for {
-					client := &http.Client{}
+					timeout := time.Duration(20 * time.Second)
+					client := &http.Client{Timeout: timeout}
 					req, _ := http.NewRequest("GET", url, nil)
 					range_header := fmt.Sprintf("bytes=%d-%d", min, max-1)
 					req.Header.Add("Range", range_header)
@@ -103,6 +105,8 @@ func main() {
 						ch <- DT{reader, min}
 						break
 					}
+
+					fmt.Print("\r!")
 				}
 
 			}
